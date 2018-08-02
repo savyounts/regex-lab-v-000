eb14d33e851bb1a147195898cd0fc86609f865cb
Sav Younts <savyounts@gmail.com>
2018-08-02 19:17:54 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,7 @@
 
 
 def starts_with_a_vowel?(word)
- if word.scan(/\b[aeiou]\w*/i) 
-   return true 
- end 
+ word.scan(/\b[aeiou]\w*/i) 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
